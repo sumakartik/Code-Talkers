422563fcd7793d2f8298a752deb12fba45599561
Suma Kartik <>
2022-03-23 14:18:32 -0400

    trying to get things working

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
+// import { render, screen } from "@testing-library/react";
+// import App from "./App";
 
-// test("renders the Code Talkers dashboard", () => {
-// }
+// // test("renders the Code Talkers dashboard", () => {
+// // }
